3f969f70a1104982cf410fbbb6c143d57ee6bd2d
mozdemirr <51960985+mozdemirr@users.noreply.github.com>
2019-07-19 11:40:30 +0300

    Weekly lesson programmy code.
    Lesson Programmy

---
+++ dersprogramı.php
@@ -0,0 +1,70 @@
+<doctype html>
+    <html>
+
+    <head>
+        <title>Tablolar</title>
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+        <table border="1" width="300px" align="center" cellpadding="20" cellspacing="10">
+            <tr>
+                <th align="center" colspan='5'> <b><center><font color=" #FF0000">DERS PROGRAMI<? echo "sa"; ?></font></center></th></b>
+            </tr>
+
+            <tr>
+
+                <th>MONDAY</th>
+                <th>TUESDAY</th>
+                <th>WEDNESDAY</th>
+                <th>THURSDAY</th>
+                <th>FRİDAY</th>
+
+            </tr>
+            <tr>
+
+                <td colspan="1" align="center">MAT</td>
+                <td colspan="1" align="center">BOŞ</td>
+                <td colspan="1" align="center">TÜKÇE</td>
+                <td colspan="1" align="center">COĞ</td>
+                <td colspan="1" align="center">TARİH</td>
+
+
+            </tr>
+            <tr>
+
+                <td colspan="1" align="center">MAT</td>
+                <td colspan="1" align="center">RESİM</td>
+                <td colspan="1" align="center">TÜKÇE</td>
+                <td colspan="1" align="center">COĞ</td>
+                <td colspan="1" align="center">MÜZİK</td>
+
+            </tr>
+            <tr>
+
+                <td colspan="1" align="center">FEN</td>
+                <td colspan="1" align="center">BEDEN</td>
+                <td colspan="1" align="center">MAT</td>
+                <td colspan="1" align="center">BOŞ</td>
+                <td colspan="1" align="center">TÜRKÇE</td>
+
+            </tr>
+            <tr>
+
+
+                <td colspan="1" align="center">FEN</td>
+                <td colspan="1" align="center">BEDEN</td>
+                <td colspan="1" align="center">MAT</td>
+                <td colspan="1" align="center">BOŞ</td>
+                <td colspan="1" align="center">TÜRKÇE</td>
+
+                </tr>
+                <tr>
+
+
+
+                </tr>
+            </table>
+        </body>
+
+        </html>
